2879ed7e26dcc8a82ea40748cabe1be06bfc81da
akajeudii <akajeudii@posteo.net>
2025-03-26 00:18:27 +0100

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -2,18 +2,18 @@ import XMonad
 import XMonad.Util.EZConfig(additionalKeysP)
 import Graphics.X11.ExtraTypes.XF86
 
-main = do
-  xmonad $ def
+main :: IO ()
+main = xmonad $ def
     { terminal    = myTerminal
     , modMask     = myModMask
     , borderWidth = myBorderWidth
     }
   `additionalKeysP`
-       [ ("M-p"                         , spawn "rofi -show drun"           )
-       , ((0, xF86XK_AudioLowerVolume  ), spawn "amixer set PCM 10%-"       )
-       , ((0, xF86XK_AudioRaiseVolume  ), spawn "amixer set PCM 10%+"       )
-       , ((0, xF86XK_AudioMute         ), spawn "amixer set IEC958,3 toggle")
-       ]
+     [ ("M-p"                         , spawn "rofi -show drun"           )
+     , ((0, xF86XK_AudioLowerVolume  ), spawn "amixer set PCM 10%-"       )
+     , ((0, xF86XK_AudioRaiseVolume  ), spawn "amixer set PCM 10%+"       )
+     , ((0, xF86XK_AudioMute         ), spawn "amixer set IEC958,3 toggle")
+     ]
 
 myTerminal    = "alacritty"
 myModMask     = mod4Mask
